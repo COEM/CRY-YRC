62301f79e291a63a1a1d580b9295d7e654e1a6a2
ibnuul <ibnuul@gmail.com>
2017-05-24 19:46:56 +0700

    Upload di Github

---
+++ src/rsa_kriptografi/KIY_Kriptografi.java
@@ -0,0 +1,39 @@
+package rsa_kriptografi;
+
+public class KIY_Kriptografi {
+
+    static String[][] data = new String [10][10];
+    
+    public static String[][] initKey (){
+        data[2][3] = "0x22165";
+        data[4][4] = "1x86643";
+        data[4][6] = "0x6355";
+        data[5][6] = "0x265";
+        data[6][6] = "0x54";
+        data[7][6] = "1x3156";
+        data[8][6] = "0x456";
+        data[9][6] = "2x16685";
+        data[0][7] = "2x20744";
+        data[1][7] = "1x15491";
+        data[2][7] = "1x5875";
+        data[3][7] = "2x15740";
+        data[4][7] = "2x20327";
+        data[5][7] = "3x234190";
+        data[6][7] = "2x14";
+        data[7][7] = "0x1737";
+        data[8][7] = "2x11102";
+        data[9][7] = "1x7462";
+        data[0][8] = "0x317";
+        data[1][8] = "1x29714";
+        data[2][8] = "2x223748";
+        data[3][8] = "1x5748";
+        data[4][8] = "2x214264";
+        data[5][8] = "2x18938";
+        data[6][8] = "2x3764";
+        data[7][8] = "2x12996";
+        data[8][8] = "0x336";
+        data[9][8] = "4x229";
+        data[0][9] = "1x5625";
+        return data;        
+    }    
+}
